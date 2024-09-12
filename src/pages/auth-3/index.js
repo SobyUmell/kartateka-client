e0fa4fc3830af9a56253f7d9a6799caf99b55dce
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2024-09-12 23:31:09 +0300

    fix: удалил лишний скопированный код

--- src/pages/auth-3/index.js
+++ src/pages/auth-3/index.js
@@ -1,21 +1,9 @@
 import { DirectionButton } from "../../shared/ui";
 import { PageProgressBar } from "../../shared/ui";
-import { Input } from "../../shared/ui";
-import { atsign, lock, city, user } from "../../shared/assets";
 import styles from "./style.module.scss";
-import { useState } from "react";
-export const Auth3 = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [name, setName] = useState("");
-  const [town, setTown] = useState("");
-  const object = {
-    values: ["Почта", "Пароль", "Имя", "Город"],
-    svgs: [atsign, lock, user, city],
-    setStates: [setEmail, setPassword, setName, setTown],
-    states: [email, password, name, town],
-  };
 
+export const Auth3 = () => {
+  
   return (
     <div className={styles.wrapper}>
       <div className={styles.blur}>
