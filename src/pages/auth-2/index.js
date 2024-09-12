05f175daf749efada69c1d1321b4929f1922c245
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2024-09-12 16:14:50 +0300

    fix: добавил value={state}

--- src/pages/auth-2/index.js
+++ src/pages/auth-2/index.js
@@ -1,24 +1,21 @@
 import { DirectionButton } from "../../shared/ui";
 import { PageProgressBar } from "../../shared/ui";
 import { Input } from "../../shared/ui";
-import { atsign,lock,city,user } from "../../shared/assets";
+import { atsign, lock, city, user } from "../../shared/assets";
 import styles from "./style.module.scss";
 import { useState } from "react";
 export const Auth2 = () => {
-    const object={
-        values:['Почта','Пароль','Имя','Город'],
-        svgs:[atsign,lock,user,city]
-    }
-    const [email,setEmail]=useState('')
-    const [password,setPassword]=useState('')
-    const [name,setName]= useState('')
-    const [town,setTown]=useState('')
-    const states = {
-        0: setEmail,
-        1: setPassword,
-        2: setName,
-        3: setTown
-      };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [town, setTown] = useState("");
+  const object = {
+    values: ["Почта", "Пароль", "Имя", "Город"],
+    svgs: [atsign, lock, user, city],
+    setStates: [setEmail, setPassword, setName, setTown],
+    states: [email, password, name, town],
+  };
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.blur}>
@@ -26,8 +23,16 @@ export const Auth2 = () => {
         <div className={styles.content}>
           <h2 className={styles.header}>Создание аккаунта</h2>
           <div className={styles.inputWrapper}>
-            {[...new Array(4)].map((value,index)=>{
-                return <Input setState={states[index]} svg={object.svgs[index]} key={index} text={object.values[index]} />
+            {[...new Array(4)].map((value, index) => {
+              return (
+                <Input
+                  setState={object.setStates[index]}
+                  state={object.states[index]}
+                  svg={object.svgs[index]}
+                  key={index}
+                  text={object.values[index]}
+                />
+              );
             })}
           </div>
           <div className={styles.groupButton}>
--- src/shared/ui/input/index.js
+++ src/shared/ui/input/index.js
@@ -1,10 +1,10 @@
 import classes from "./style.module.scss";
-export const Input = ({setState, svg, text, ...props }) => {
+export const Input = ({setState,state, svg, text, ...props }) => {
   return (
     <div className={classes.wrapper}>
       <img className={classes.img} src={svg}></img>
       <p className={classes.text}>{text}</p>
-      <input onChange={(e)=>{setState(e.target.value)}} className={classes.input}></input>
+      <input value={state} onChange={(e)=>{setState(e.target.value)}} className={classes.input}></input>
     </div>
   );
 };
