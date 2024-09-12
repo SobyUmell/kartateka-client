c0d03f63a915e02ae859e1183b91340487a7bc5c
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2024-09-12 15:30:51 +0300

    добавил стейты, привязал к инпутам

--- src/pages/auth-2/index.js
+++ src/pages/auth-2/index.js
@@ -3,11 +3,22 @@ import { PageProgressBar } from "../../shared/ui";
 import { Input } from "../../shared/ui";
 import { atsign,lock,city,user } from "../../shared/assets";
 import styles from "./style.module.scss";
+import { useState } from "react";
 export const Auth2 = () => {
     const object={
         values:['Почта','Пароль','Имя','Город'],
-        svgs:[atsign,lock,city,user]
+        svgs:[atsign,lock,user,city]
     }
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+    const [name,setName]= useState('')
+    const [town,setTown]=useState('')
+    const states = {
+        0: setEmail,
+        1: setPassword,
+        2: setName,
+        3: setTown
+      };
   return (
     <div className={styles.wrapper}>
       <div className={styles.blur}>
@@ -15,8 +26,8 @@ export const Auth2 = () => {
         <div className={styles.content}>
           <h2 className={styles.header}>Создание аккаунта</h2>
           <div className={styles.inputWrapper}>
-            {[...new Array(4)].map((valuue,index)=>{
-                return <Input svg={object.svgs[index]} key={index} text={object.values[index]} />
+            {[...new Array(4)].map((value,index)=>{
+                return <Input setState={states[index]} svg={object.svgs[index]} key={index} text={object.values[index]} />
             })}
           </div>
           <div className={styles.groupButton}>
--- src/shared/ui/input/index.js
+++ src/shared/ui/input/index.js
@@ -1,10 +1,10 @@
 import classes from "./style.module.scss";
-export const Input = ({ svg, text, ...props }) => {
+export const Input = ({setState, svg, text, ...props }) => {
   return (
     <div className={classes.wrapper}>
       <img className={classes.img} src={svg}></img>
       <p className={classes.text}>{text}</p>
-      <input className={classes.input}></input>
+      <input onChange={(e)=>{setState(e.target.value)}} className={classes.input}></input>
     </div>
   );
 };
