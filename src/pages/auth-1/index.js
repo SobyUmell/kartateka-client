a63f292b182ea801704ff8dac27c70e3b01d03e5
n3v3sl33p <ar.yatsunencko@yandex.ru>
2024-09-12 08:40:03 +0300

    delo sdelano

--- src/pages/auth-1/index.js
+++ src/pages/auth-1/index.js
@@ -5,25 +5,20 @@ import styles from "./style.module.scss";
 export function Auth1() {
   return (
     <div className={styles.wrapper}>
-      <PageProgressBar pageNumber={1} />
-
-      <img src={finger} alt="finger" className={styles.img} />
-      <h2 className={styles.header}>Защитите свои данные</h2>
-      <p className={styles.paragraph}>
-        С помощью передовой системы авторизации{" "}
-        <span className={styles.span}>concept id</span>
-      </p>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          paddingLeft: "22px",
-          paddingRight: "22px",
-          alignContent: "center",
-        }}
-      >
-        <DirectionButton text={"Назад"} direction={0} />
-        <DirectionButton text={"Продолжить"} direction={1} />
+      <div className={styles.blur}>
+        <PageProgressBar pageNumber={1} />
+        <div className={styles.content}>
+          <img src={finger} alt="finger" className={styles.img} />
+          <h2 className={styles.header}>Защитите свои данные</h2>
+          <p className={styles.paragraph}>
+            С помощью передовой системы авторизации{" "}
+            <span className={styles.span}>concept id</span>
+          </p>
+          <div className={styles.groupButton}>
+            <DirectionButton text={"Назад"} direction={0} />
+            <DirectionButton text={"Продолжить"} direction={1} />
+          </div>
+        </div>
       </div>
     </div>
   );
