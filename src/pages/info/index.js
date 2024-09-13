6b81011a8647953683d5f1691f75eee7e68d9837
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2024-09-13 23:39:13 +0300

    fix: исправил местоположение надписей feat: инициализировал страницу info

--- src/pages/info/index.js
+++ src/pages/info/index.js
@@ -1,9 +1,9 @@
+import styles from "./style.module.scss";
 const Index = (props) => {
     
-
     return (
-        <>
+        <div className={styles.wrapper}>
             
-        </>
+        </div>
     )
 }
