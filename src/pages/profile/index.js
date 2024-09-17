530c36316db86e383838637c7826922be01f31fa
n3v3sl33p <ar.yatsunencko@yandex.ru>
2024-09-17 12:20:23 +0300

    nav-bar

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,8 +1,13 @@
+import React from "react";
 import { NavBar, WaveBackground } from "../../shared/ui";
 import styles from "./style.module.scss";
 export const Profile = () => {
+  const [file, setFile] = React.useState(null);
+
   return (
     <WaveBackground>
+      <input type="file" value={image} />
+      <img src={image} alt="error" />
       <NavBar />
     </WaveBackground>
   );
--- src/shared/ui/nav-bar/index.js
+++ src/shared/ui/nav-bar/index.js
@@ -2,11 +2,25 @@ import styles from "./style.module.scss";
 import { mapIcon, houseIcon, coffeeIcon, penIcon } from "../../assets";
 export const NavBar = () => {
   return (
-    <ul className={styles.list}>
-      <li className={styles.item}>
-        <img src={mapIcon} alt="map" className={styles.mapIcon} />
-        <span>Главная</span>
-      </li>
-    </ul>
+    <div className={styles.wrapper}>
+      <ul className={styles.list}>
+        <li className={styles.item}>
+          <img src={houseIcon} alt="map" className={styles.mapIcon} />
+          <span className={styles.span}>Главная</span>
+        </li>
+        <li className={styles.item}>
+          <img src={mapIcon} alt="map" className={styles.mapIcon} />
+          <span className={styles.span}>Главная</span>
+        </li>
+        <li className={styles.item}>
+          <img src={penIcon} alt="map" className={styles.mapIcon} />
+          <span className={styles.span}>Главная</span>
+        </li>
+        <li className={styles.item}>
+          <img src={coffeeIcon} alt="map" className={styles.mapIcon} />
+          <span className={styles.span}>Главная</span>
+        </li>
+      </ul>
+    </div>
   );
 };
