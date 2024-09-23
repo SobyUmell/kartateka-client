d753128423f562e8e76c55c03f2ade0e5cb8b969
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2024-09-23 23:56:18 +0300

    feat: добавил see more и кнопку выхода из истории

--- src/pages/teka/index.jsx
+++ src/pages/teka/index.jsx
@@ -1,32 +1,41 @@
-import { NavBar, WaveBackground } from "../../shared/ui";
+import { Button, NavBar, WaveBackground } from "../../shared/ui";
 import { TekaInput } from "../../shared/ui";
 import styles from "./style.module.scss";
 import { Search } from "../../shared/assets";
 import { TekaWidget } from "../../widgets";
 import { useState } from "react";
 import Stories from "react-insta-stories";
+
 export const Teka = () => {
   const [flag, setFlag] = useState(false);
+  const seeMoreContent = () => {
+    return (
+      <div style={{ display: "flex", width: "100%", justifyContent: "center" }}>
+        <Button onclick={() => setFlag(false)} span={"exit"}></Button>
+      </div>
+    );
+  };
   const stories = [
     {
       url: "https://steamuserimages-a.akamaihd.net/ugc/1752456213847927038/87962D92C69DB797C2AC86972DF19A6D9AAC46A2/?imw=512&amp;imh=288&amp;ima=fit&amp;impolicy=Letterbox&amp;imcolor=%23000000&amp;letterbox=true",
       duration: 1500,
+      seeMore: seeMoreContent,
     },
     {
       url: "https://sun9-60.userapi.com/impg/c857332/v857332443/ecb77/4GY2V_AOoaA.jpg?size=769x589&quality=96&sign=dc95a1fc094e8a9282dc6751fb099f5c&c_uniq_tag=h7S6cZNH8NgEM9fvbp1yBnXm2CzE7NKzzrNF2cpcfwI&type=album",
       duration: 1500,
+      seeMore: seeMoreContent,
     },
     {
       url: "https://avatars.dzeninfra.ru/get-zen_doc/1589949/pub_5cb1a44f7c45ef00b542e21e_5cb1a51568a9c800b4380ab8/scale_1200",
       duration: 1500,
+      seeMore: seeMoreContent,
     },
   ];
   const clickHandler = () => {
     setFlag(true);
   };
-  function redirectToHome() {
-    history.push("/");
-  }
+
   return (
     <>
       {flag ? (
--- src/shared/ui/button/index.jsx
+++ src/shared/ui/button/index.jsx
@@ -1,7 +1,12 @@
 import styles from "./style.module.scss";
-export const Button = ({ span, children, ...props }) => {
+export const Button = ({ onclick, style, span, children, ...props }) => {
   return (
-    <button className={styles.button} {...props}>
+    <button
+      onClick={onclick}
+      className={styles.button}
+      style={style}
+      {...props}
+    >
       <span className={styles.spanF}>{span + " "} </span>
       <span className={styles.spanS}>{children}</span>
     </button>
