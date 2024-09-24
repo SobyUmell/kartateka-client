0d2dc9eeb48565de7f3aa5d681334d758ec09284
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2024-09-24 14:19:21 +0300

    fix: изменил defaultInterval с 1500 на 2000 fix: добавил padding 15px

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <ScreenContainer>
-        <Profile />
+        <Teka />
       </ScreenContainer>
     </div>
   );
--- src/pages/teka/index.jsx
+++ src/pages/teka/index.jsx
@@ -18,7 +18,7 @@ export const Teka = () => {
         }}
       >
         <Button
-          style={{}}
+          style={{ padding: "15px" }}
           spanStyle={{ marginRight: "0px" }}
           onclick={() => setFlag(false)}
           span={"X"}
@@ -29,17 +29,14 @@ export const Teka = () => {
   const stories = [
     {
       url: "https://steamuserimages-a.akamaihd.net/ugc/1752456213847927038/87962D92C69DB797C2AC86972DF19A6D9AAC46A2/?imw=512&amp;imh=288&amp;ima=fit&amp;impolicy=Letterbox&amp;imcolor=%23000000&amp;letterbox=true",
-      duration: 1500,
       seeMore: seeMoreContent,
     },
     {
       url: "https://sun9-60.userapi.com/impg/c857332/v857332443/ecb77/4GY2V_AOoaA.jpg?size=769x589&quality=96&sign=dc95a1fc094e8a9282dc6751fb099f5c&c_uniq_tag=h7S6cZNH8NgEM9fvbp1yBnXm2CzE7NKzzrNF2cpcfwI&type=album",
-      duration: 1500,
       seeMore: seeMoreContent,
     },
     {
       url: "https://avatars.dzeninfra.ru/get-zen_doc/1589949/pub_5cb1a44f7c45ef00b542e21e_5cb1a51568a9c800b4380ab8/scale_1200",
-      duration: 1500,
       seeMore: seeMoreContent,
     },
   ];
@@ -52,7 +49,7 @@ export const Teka = () => {
       {flag ? (
         <Stories
           stories={stories}
-          defaultInterval={1500}
+          defaultInterval={2000}
           width={"100%"}
           height={"100%"}
           loop={false}
