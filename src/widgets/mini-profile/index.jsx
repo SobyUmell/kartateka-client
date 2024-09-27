1c9475df189430c3af6635c8a612f0ce7fe1ff33
n3v3sl33p <ar.yatsunencko@yandex.ru>
2024-09-27 23:05:50 +0300

    fix after code review

--- src/widgets/mini-profile/index.jsx
+++ src/widgets/mini-profile/index.jsx
@@ -4,6 +4,8 @@ import { IconButton } from "../../shared/ui/icon-button";
 import { geoDataIcon, likeIcon, shareIcon } from "../../shared/assets";
 import { getHeightMiniProfile } from "../../shared/lib/getHeightMiniProfile";
 import { useState, useRef } from "react";
+import { TekaWidget } from "../../widgets";
+
 export const MiniProfile = ({ img }) => {
   const [height, setHeight] = useState(50);
   const startTouchY = useRef(0);
@@ -19,7 +21,7 @@ export const MiniProfile = ({ img }) => {
     const deltaY = startTouchY.current - currentTouchY;
 
     const newHeight = Math.min(
-      Math.max(startHeight.current + (deltaY / window.innerHeight) * 100, 50),
+      Math.max(startHeight.current + (deltaY / window.innerHeight) * 100, 0),
       100
     );
     setHeight(newHeight);
@@ -30,40 +32,80 @@ export const MiniProfile = ({ img }) => {
       setHeight(75);
     } else if (height > 75 && height < 100) {
       setHeight(100);
+    } else if (height > 25 && height < 50) {
+      setHeight(50);
+    } else {
+      setHeight(0);
     }
   };
-  return (
-    <div
-      style={getHeightMiniProfile(height)}
-      className={styles.container}
-      onTouchStart={handleTouchStart}
-      onTouchMove={handleTouchMove}
-      onTouchEnd={handleTouchEnd}
-    >
-      <div className={styles.header}>
-        {img ? (
-          <img className={styles.avatar} src={img} alt="profile avatar" />
-        ) : (
-          <div className={styles.grayCircle} />
-        )}
-        <div>
-          <h2 className={styles.title}>CONCEPT</h2>
-          <div className={styles.info}>
-            <span className={styles.typePlace}>{miniProfileObj.coffee}</span>
-            <span className={styles.address}>Таганрог/ Чехова 324</span>
-          </div>
-          <div className={styles.groupButton}>
-            <IconButton icon={likeIcon} withBackground={true} />
-            <IconButton icon={shareIcon} withBackground={true} />
-            <IconButton icon={geoDataIcon} withBackground={true} />
+
+  const handleReturnBarTouchStart = (e) => {
+    startTouchY.current = e.touches[0].clientY;
+    startHeight.current = 0;
+  };
+
+  const handleReturnBarTouchMove = (e) => {
+    const currentTouchY = e.touches[0].clientY;
+    const deltaY = startTouchY.current - currentTouchY;
+
+    const newHeight = Math.min(
+      Math.max(startHeight.current + (deltaY / window.innerHeight) * 100, 0),
+      50
+    );
+    setHeight(newHeight);
+  };
+
+  const handleReturnBarTouchEnd = () => {
+    if (height > 0) {
+      setHeight(50);
+    }
+  };
+
+  if (height === 0) {
+    return (
+      <div
+        className={styles.widgetReturnBar}
+        onTouchStart={handleReturnBarTouchStart}
+        onTouchMove={handleReturnBarTouchMove}
+        onTouchEnd={handleReturnBarTouchEnd}
+      >
+        <div />
+      </div>
+    );
+  } else {
+    return (
+      <div
+        style={getHeightMiniProfile(height)}
+        className={styles.container}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+      >
+        <div className={styles.header}>
+          {img ? (
+            <img className={styles.avatar} src={img} alt="profile avatar" />
+          ) : (
+            <div className={styles.grayCircle} />
+          )}
+          <div>
+            <h2 className={styles.title}>CONCEPT</h2>
+            <div className={styles.info}>
+              <span className={styles.typePlace}>{miniProfileObj.coffee}</span>
+              <span className={styles.address}>Таганрог/ Чехова 324</span>
+            </div>
+            <div className={styles.groupButton}>
+              <IconButton icon={likeIcon} withBackground={true} />
+              <IconButton icon={shareIcon} withBackground={true} />
+              <IconButton icon={geoDataIcon} withBackground={true} />
+            </div>
           </div>
         </div>
+        <div className={styles.content}>
+          <TekaWidget />
+          <TekaWidget />
+          <TekaWidget />
+        </div>
       </div>
-      <div className={styles.content}>
-        <div className={styles.contentItem} />
-        <div className={styles.contentItem} />
-        <div className={styles.contentItem} />
-      </div>
-    </div>
-  );
+    );
+  }
 };
