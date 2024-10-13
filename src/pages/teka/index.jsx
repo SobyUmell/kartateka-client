a1839bf785b4d687565a6d19036c93664c14004e
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2024-10-13 18:14:47 +0300

    feat: implemented the transfer of components in story

--- src/pages/teka/index.jsx
+++ src/pages/teka/index.jsx
@@ -5,44 +5,69 @@ import { Search } from "../../shared/assets";
 import { TekaWidget } from "../../widgets";
 import { useState, useEffect } from "react";
 import Stories from "react-insta-stories";
-
+let object = {
+  // объект, который будет ответственнен за контент, который передается в виджет для отрисовки
+  postId: "",
+  organizationId: "",
+  views: "",
+  createdAt: "",
+  updatedAt: "",
+  media: "",
+  title: "",
+  text: "",
+  tags: "",
+  img: "",
+  content: "",
+};
+const seeMoreContent = () => {
+  // контент отображаемый при нажатии на seeMore
+  return (
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        justifyContent: "right",
+      }}
+    >
+      <Button
+        style={{ padding: "15px" }}
+        spanStyle={{ marginRight: "0px" }}
+        onclick={() => setFlag(false)}
+        span={"X"}
+      ></Button>
+    </div>
+  );
+};
+let masTitle = ["История 0", "История 1", "История 2"];
+let masText;
+const widgetContent = ({ i, title }) => {
+  // контент истории
+  return (
+    <div className={styles.storyContainer}>
+      <h2 className={styles.title}>{title}</h2>
+      {!title ? seeMoreContent() : <></>}
+    </div>
+  );
+};
+const stories = [];
 export const Teka = () => {
   const [flag, setFlag] = useState(false);
-  const seeMoreContent = () => {
-    return (
-      <div
-        style={{
-          position: "absolute",
-          top: "10px",
-          right: "5px",
-        }}
-      >
-        <Button
-          style={{ padding: "15px" }}
-          spanStyle={{ marginRight: "0px" }}
-          onclick={() => setFlag(false)}
-          span={"X"}
-        ></Button>
-      </div>
-    );
-  };
-  const stories = [
-    {
-      url: "https://steamuserimages-a.akamaihd.net/ugc/1752456213847927038/87962D92C69DB797C2AC86972DF19A6D9AAC46A2/?imw=512&amp;imh=288&amp;ima=fit&amp;impolicy=Letterbox&amp;imcolor=%23000000&amp;letterbox=true",
-      seeMore: seeMoreContent,
-    },
-    {
-      url: "https://sun9-60.userapi.com/impg/c857332/v857332443/ecb77/4GY2V_AOoaA.jpg?size=769x589&quality=96&sign=dc95a1fc094e8a9282dc6751fb099f5c&c_uniq_tag=h7S6cZNH8NgEM9fvbp1yBnXm2CzE7NKzzrNF2cpcfwI&type=album",
-      seeMore: seeMoreContent,
-    },
-    {
-      url: "https://avatars.dzeninfra.ru/get-zen_doc/1589949/pub_5cb1a44f7c45ef00b542e21e_5cb1a51568a9c800b4380ab8/scale_1200",
-      seeMore: seeMoreContent,
-    },
-  ];
-  const clickHandler = () => {
-    setFlag(true);
-  };
+  const [len, setLen] = useState(0);
+  useEffect(() => {
+    // фук предназначенный для вытягивания инфрормации об историях
+    setLen(3);
+  }, []);
+  useEffect(() => {
+    // реализована загрузка историй из массива историй
+    for (let i = 0; i < len; i++) {
+      object = {
+        content: () => widgetContent({ i: i, title: masTitle[i] }),
+        postId: i,
+      };
+      stories.push(object);
+      console.log("история загружена ", { i });
+    }
+  }, [len]);
 
   return (
     <>
