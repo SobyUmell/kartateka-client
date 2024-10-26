68fce04414bebe997faa83dd0bf70da1b93dbb5d
Sobar <sdruza4@gmail.com>
2024-10-26 23:19:24 +0300

    add: Initialized a map page and installed leaflet and leaflet-react libraries.

---
+++ src/pages/map/index.jsx
@@ -0,0 +1,20 @@
+import s from "./map.module.scss";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
+export const MapPage = () => {
+  return (
+    <div className={s.map_page}>
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+      ,
+    </div>
+  );
+};
