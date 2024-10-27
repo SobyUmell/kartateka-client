cfe3189e2638fd91e70a3053ef04f95aeb631d46
Sobar <sdruza4@gmail.com>
2024-10-27 14:46:42 +0300

    feat: configured the map and got rid of the watermark

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,4 +3,5 @@ export { Auth2 } from "./auth-2/index";
 export { Auth3 } from "./auth-3/index";
 export { Info } from "./info/index";
 export { Profile } from "./profile/index";
-export {Teka} from "./teka/index"
+export { Teka } from "./teka/index";
+export { MapPage } from "./map/index";
--- src/pages/map/index.jsx
+++ src/pages/map/index.jsx
@@ -1,21 +1,13 @@
-import s from "./map.module.scss";
 import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import "../../shared/styles/leaflet-override.css";
+import s from "./map.module.scss";
+import "leaflet/dist/leaflet.css";
 export const MapPage = () => {
   // I know where you live, your IP and MAC addresses are next
   const position = [47.212216916047225, 38.915121793521074];
   return (
-    <div className={s.map_page}>
-      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </div>
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+    </MapContainer>
   );
 };
