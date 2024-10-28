349002d54370f59e3e33ea81199487efd4052ffa
Sobar <sdruza4@gmail.com>
2024-10-28 14:26:23 +0300

    feat: Changed map. Now it has a dark mode and the only place that can be explored so far is Taganrog

--- src/pages/map/index.jsx
+++ src/pages/map/index.jsx
@@ -1,13 +1,32 @@
-import { MapContainer, TileLayer, useMap } from "react-leaflet";
-import "../../shared/styles/leaflet-override.css";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "../../shared/styles/leaflet-override.scss";
 import s from "./map.module.scss";
 import "leaflet/dist/leaflet.css";
+import { useState } from "react";
 export const MapPage = () => {
   // I know where you live, your IP and MAC addresses are next
   const position = [47.212216916047225, 38.915121793521074];
+  const bounds = [
+    [47.15759108614136, 38.817677182933565],
+    [47.30454649974391, 39.011686089791],
+  ];
+  const [pointer, setPointer] = useState("Light Mode");
+
+  const handleSwitch = (event) => {
+    setPointer(event.target.value);
+    console.log(pointer == "Dark Mode");
+  };
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-    </MapContainer>
+    <div className={s.map_page}>
+      <MapContainer
+        center={position}
+        zoom={13}
+        scrollWheelZoom={false}
+        maxBounds={bounds}
+        maxBoundsViscosity={bounds}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </MapContainer>
+    </div>
   );
 };
