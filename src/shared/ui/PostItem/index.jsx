2702fe53936af36c4fb2226828688d914241e1c0
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2024-11-04 16:25:25 +0300

    fix: add alt for teka widget in teka page

--- src/shared/ui/PostItem/index.jsx
+++ src/shared/ui/PostItem/index.jsx
@@ -5,7 +5,7 @@ export const PostItem = ({ text, img, clickHandler, children, ...props }) => {
     <div className={styles.div}>
       <div className={styles.sliderContainerCover} onClick={clickHandler}>
         <img className={styles.img} src={img} alt="фото компании" />
-        <h2 className={styles.h2} style={{ marginTop: "15%" }}>{text}</h2>
+        <h2 className={styles.h2} style={{ marginTop: "15%" }}>{text? text: 'название компании'}</h2>
       </div>
     </div>
   );
